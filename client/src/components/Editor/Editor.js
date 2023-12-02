e4c27bbe5f915bb2cab3ddad788c96c712b1dd14
RoseLiu <liu.rose@wustl.edu>
2023-12-01 18:11:54 -0600

    login/reg + components

---
+++ client/src/components/Editor/Editor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Quill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function Editor () {
+    const [content, setContent] = React.useState(' ');
+
+   
+}
