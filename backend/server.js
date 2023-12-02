0180e19b900886516d526ff04cf839e9fff6b3ce
ShawnZhu03 <shawn.z@wustl.edu>
2023-12-02 17:01:08 -0600

    user model

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,28 @@
 //server
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB conneciton 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://shawnz:J3upIUKNN3STGEli@330final.q7jvbsq.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri);
+
+const connection = mongoose.connection; 
+connection.once('open', () => {
+  console.log("mongoose databse connection established successfully");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`server is running on: ${port}`);
+})
 
 console.log("hello");
